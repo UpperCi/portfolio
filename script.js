73e15d98886d40f7f999db176d04d2fd8e9b0f2a
UpperCi <daniverschoor@outlook.com>
2022-06-28 00:26:54 +0200

    added project view, polish

--- script.js
+++ script.js
@@ -6,31 +6,36 @@ function updateContent(content) {
 	contentTarget.innerHTML = content;
 	contentWindow.classList.remove("inactive");
 	contentWindow.classList.add("active");
+	
+	const projectThumbnails = document.querySelectorAll(".project-view-button");
+	for (let thumb of projectThumbnails) {
+		const target = thumb.getAttribute('target');
+		const nav = navObjs.find(el => el.getAttribute('target') == target);
+		thumb.onclick = () => nav.click();
+		thumb.onmouseenter = () => nav.firstElementChild.focus();
+		thumb.onmouseleave = () => nav.firstElementChild.blur();
+	}
 }
 
 function fetchContent(node) {
-	for (let nav of navObjs) {
-		nav.classList.remove("current");
-	}
-	node.classList.add("current")
+	navObjs.forEach(nav => nav.classList.remove("current"));
+	node.classList.add("current");
 	const page = node.getAttribute('target');
 	fetch(`content/${page}.html`)
 	.then(response => response.text())
-	.then(content => updateContent(content))
+	.then(content => updateContent(content));
 }
 
 function closeWindow() {
-	document.querySelector(".current").classList.remove("current");
+	navObjs.forEach(nav => nav.classList.remove("current"));
 	contentWindow.classList.remove("active");
 	contentWindow.classList.add("inactive");
 }
 
 function init() {
 	contentWindow = document.querySelector(".window");
-	navObjs = document.querySelectorAll("#nav ul");
-	for (let nav of navObjs) {
-		nav.addEventListener('click', () => fetchContent(nav));
-	}
+	navObjs = Array.from(document.querySelectorAll("#nav ul"));
+	navObjs.forEach(nav => nav.onclick = () => fetchContent(nav));
 	
 	document.querySelector(".exit").addEventListener("click", () => closeWindow());
 }
